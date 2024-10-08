c6d28461f4e3425188c5c3e645c60a03126faede
FestimTheA <festim@theacompany.xyz>
2024-10-08 02:29:57 +0200

    First draft of sidebar

--- sales-ai-agent/app/about/layout.tsx
+++ sales-ai-agent/app/about/layout.tsx
@@ -1,11 +1,11 @@
-export default function AboutLayout({
+export default function DocsLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <section className="flex flex-col items-start justify-center gap-4 py-8 md:py-10">
-      <div className="inline-block max-w-lg text-center justify-center">
+    <section className="flex flex-col">
+      <div className="inline-block">
         {children}
       </div>
     </section>
